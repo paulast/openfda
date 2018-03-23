830a7c0c904cbac347d42a7e9e93209e582432c9
psanchez <paulast1999@gmail.com>
2018-03-23 18:09:27 +0100

    Práctica1

--- openfda-1/test.py
+++ openfda-1/test.py
@@ -3,7 +3,7 @@ import json
 
 headers = {'User-Agent': 'http-client'}
 
-conn = http.client.HTTPSConnection("api.github.com")
+conn = http.client.HTTPSConnection("api.fda.gov")
 conn.request("GET", "/drug/label.json", None, headers)
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
@@ -14,6 +14,8 @@ repos = json.loads(repos_raw)
 
 repo = repos["results"][0]
 print("El id del medicamento es", repo["id"])
+print("El propósito del producto es", repo["purpose"])
+print("El nombre del fabricante es", repo["openfda"]["manufacturer_name"])
 
     
 
