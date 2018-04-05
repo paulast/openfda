bd87a7785871f5dbda282ccb25106f586f80359d
paulast <37545299+paulast@users.noreply.github.com>
2018-04-05 22:38:55 +0200

    Update práctica3.py

--- openfda-3/práctica3.py
+++ openfda-3/práctica3.py
@@ -3,52 +3,41 @@ import json
 import http.server
 import socketserver
 
+
 PORT = 8020
 
+
+headers = {'User-Agent': 'http-client'}
+
+conn = http.client.HTTPSConnection("api.fda.gov")
+conn.request("GET", "/drug/label.json?limit=10", None, headers)
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+repos_raw = r1.read().decode("utf-8")
+conn.close()
+
+repos = json.loads(repos_raw)
+for i in range(len(repos["results"])):
+    info=repos["results"][i]
+    
+    
 class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
-
-    headers = {'User-Agent': 'http-client'}
-
-    conn = http.client.HTTPSConnection("api.fda.gov")
-    conn.request("GET", "/drug/label.json?limit=10", None, headers)
-    r1 = conn.getresponse()
-    print(r1.status, r1.reason)
-    repos_raw = r1.read().decode("utf-8")
-    conn.close()
-
-    repos = json.loads(repos_raw)
-
-    for i in range(len(repos["results"])):
-        info=repos["results"][i]
-
+    
     def do_GET(self):
-
-        # -- Envio de la respuesta al cliente
 
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
+        content = "<html><body>"
+        
+        if info["openfda"]:
+            print("El nombre del medicamento es", info["openfda"]["generic_name"][0])
 
-
-        mensaje = """
-        <!DOCTYPE>
-        <html>
-        <body>
-        """
-
-        mensaje += "<p>" + "Información sobre medicamentos" + "</p>"
-        mensaje += "<ul>"
-        mensaje += "<li>Recurso solicitado: {}</li>".format(self.path)
-        mensaje += "</ul>"
-        mensaje += "</body>"
-        mensaje += "</html>"
-        mensaje += info
         
         self.wfile.write(bytes(mensaje, "utf8"))
-        print("Petición atendida!")
         return
 
-
+    
 Handler = testHTTPRequestHandler
 
 httpd = socketserver.TCPServer(("", PORT), Handler)
