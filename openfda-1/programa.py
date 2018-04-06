80d592e52ae49f5efb0cfb7b2468e6a88095a3e8
paulast <37545299+paulast@users.noreply.github.com>
2018-04-06 22:01:31 +0200

    Update programa.py

--- openfda-1/programa.py
+++ openfda-1/programa.py
@@ -4,17 +4,22 @@ import json
 
 headers = {'User-Agent': 'http-client'}
 
-#Ahora nos conectaremos con la página api de fda para poder descargarnos el medicamento.
+#Ahora nos conectaremos con la página api de fda para poder descargarnos la información del medicamento.
 conn = http.client.HTTPSConnection("api.fda.gov")
 conn.request("GET", "/drug/label.json", None, headers)
+#Almacenamos la información en la variable r1.
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
+#Decodificamos la información obtenida para poder leerlo correctamente.
 repos_raw = r1.read().decode("utf-8")
 conn.close()
-
+#En la variable repos guardamos toda la información del archivo descargado y decodificado.
 repos = json.loads(repos_raw)
 
+#La variable repo la asociamos solamente a la información que aparece en el apartado "results",
+#que es el que nos interesa.
 repo = repos["results"][0]
+#Imprimimos por pantalla la información que nos indican en el ejercicio. 
 print("El id del medicamento es", repo["id"])
 print("El propósito del producto es", repo["purpose"][0])
 print("El nombre del fabricante es", repo["openfda"]["manufacturer_name"][0])
