5699fd316f9b68fec905bdbfea1b6e13441dcc7d
paulast <37545299+paulast@users.noreply.github.com>
2018-04-06 22:08:35 +0200

    Update programa2.py

--- openfda-1/programa2.py
+++ openfda-1/programa2.py
@@ -11,9 +11,14 @@ repos_raw = r1.read().decode("utf-8")
 conn.close()
 
 repos = json.loads(repos_raw)
-
+#Hasta este apartado el código es igual al del primer ejercicio, a excepción de la función "limit", utilizada para
+#conseguir la información de los diez primeros medicamentos, en lugar de uno solo.
+#Con un bucle for iteramos sobre la información que aparece en "results"
+#de cada medicamento.
 for i in range(len(repos["results"])):
+    #En la variable info guardamos esta información de cada medicamento.
     info=repos["results"][i]
+    #Imprimimos el id de cada medicamento tal y como pide el ejercicio.
     print("La id del medicamento es", info["id"])
 
 
